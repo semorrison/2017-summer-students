aa4b069dd72c73696d18f28651bc7c3635c34bc3
yuxiliu <u5950011@anu.edu.au>
2017-11-29 05:53:19 +1100

    simple example of xnat = zero being decidable

--- Scott/20171127-decidable-example.lean
+++ Scott/20171127-decidable-example.lean
@@ -4,10 +4,11 @@ def even : ℕ → Prop
 | 0        := true
 | (succ n) := ¬ (even n)
 
-lemma even_6 : even 6 :=
+example : even 6 := by { unfold even, simp }
+
+lemma not_not : ∀ p : Prop, p → ¬¬p :=
 begin
-  unfold even,
-  simp
+  intro, intro, intro, contradiction
 end
 
 def infinitely_many_even_integers : ∀ n : ℕ, ∃ m ≥ n, even m :=
@@ -24,7 +25,7 @@ intro n,
 induction n,
 {
     unfold even,
-    exact decidable.true -- I found this by grepping for "decidable" in lean/library/, finding lots in init/logic.lean, and noticing the instance called `decidable.true` there
+    exact decidable.true
 },
 {
   cases ih_1,
@@ -35,7 +36,7 @@ induction n,
   {
     unfold even,
     refine decidable.is_false _,
-    cc -- This is a horrible hack, because I ran out of time working out the name of (p → ¬¬p).
+    apply not_not, assumption
   }
 }
 end
@@ -44,22 +45,58 @@ end
 
 def infinitely_many_even_integers_2 : ∀ n : ℕ, ∃ m ≥ n, even m :=
 begin
-intro n,
-by_cases (even n),
-{
-  existsi n,
-  existsi _,
-  assumption,
-  unfold ge,
-},
-{
-  existsi (n+1),
-  existsi _,
-  unfold even,
-  assumption,
-  -- how do we obtain a proof that n + 1 ≥ n? This is pretty gross.
-  unfold ge,
-  unfold has_le.le,
-  exact less_than_or_equal.step (less_than_or_equal.refl n),
-}
+  intro n,
+  by_cases (even n),
+  {
+    existsi n,
+    existsi _,
+    assumption,
+    unfold ge,
+  },
+  {
+    existsi (n+1),
+    existsi _,
+    unfold even,
+    assumption,
+    -- how do we obtain a proof that n + 1 ≥ n? This is pretty gross.
+    unfold ge,
+    unfold has_le.le,
+    exact less_than_or_equal.step (less_than_or_equal.refl n),
+  }
 end
+
+inductive xnat 
+| zero : xnat
+| succ : xnat → xnat
+open xnat
+
+namespace xnat
+def is_zero : xnat → Prop := (λ n, n = zero)
+
+lemma zero_not_succ (n : xnat) : zero ≠ succ n := 
+  by apply xnat.no_confusion
+lemma succ_not_zero (n : xnat) : succ n ≠ zero := 
+begin
+  intro, 
+  have : ¬(zero = succ n), from zero_not_succ n,
+  exact this a.symm
+end
+
+instance zero_decidable : decidable_pred is_zero :=
+begin
+  unfold decidable_pred,
+  intro a,
+  induction a with a Ha,
+  {
+    unfold is_zero,
+    simp,
+    exact decidable.true
+  },
+  {
+    unfold is_zero,
+    have : ¬(succ a = zero), by apply succ_not_zero,
+    simp [this],
+    exact decidable.false
+  }
+end
+end xnat
