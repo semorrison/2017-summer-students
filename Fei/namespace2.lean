65a1f984cd5140eb71b6dda2373058d7974b08e4
Fei Lu <33955789+felicitylu@users.noreply.github.com>
2017-11-29 16:05:16 +1100

    Add files via upload

---
+++ Fei/namespace2.lean
@@ -0,0 +1,28 @@
+namespace hide
+
+universe u
+constant list : Type u → Type u
+
+namespace lis
+  constant cons   : Π { α : Type u}, α → list α → list α
+  constant nil    : Π {α : Type u}, list α
+  constant append : Π {α : Type u}, list α → list α → list α  
+  
+end lis
+
+open hide.lis
+#check hide.lis
+#check hide.list
+-- A namespace is not an identifier and could not be checked
+
+variable  α : Type
+variable  a : α
+variables l1 l2 : list α
+
+#check cons a nil
+#check append (cons a nil) l1
+#check append (append (cons a nil) l1) l2
+
+end hide
+
+-- Alternately, we have to work in our own namespace from the beginning to the end
