d06d1a53286da62aa123bcd6256e1a16da7aeba1
Fei Lu <33955789+felicitylu@users.noreply.github.com>
2017-11-29 16:18:17 +1100

    Update namespace2.lean

--- Fei/namespace2.lean
+++ Fei/namespace2.lean
@@ -4,9 +4,9 @@ universe u
 constant list : Type u → Type u
 
 namespace lis
-  constant cons   : Π { α : Type u}, α → list α → list α
-  constant nil    : Π {α : Type u}, list α
-  constant append : Π {α : Type u}, list α → list α → list α  
+  constant cons   : Π { α : Type u }, α → list α → list α
+  constant nil    : Π { α : Type u }, list α
+  constant append : Π { α : Type u }, list α → list α → list α  
   
 end lis
 
