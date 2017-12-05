83c113b9469afd6f47f4ec91dc2821fbcfaf3921
Scott Morrison <scott@tqft.net>
2017-12-04 19:40:32 -0800

    cardinality of decreasing_lists; this would not be a good way to start

--- Scott/20171203-decreasing-sequence.lean
+++ Scott/20171203-decreasing-sequence.lean
@@ -1,3 +1,6 @@
+import tactic.norm_num
+import data.vector
+
 inductive decreasing_list : ℕ → Type 
 | nil  : decreasing_list 0
 | cons : Π (m : ℕ) {n : ℕ} (p : m ≥ n) (l : decreasing_list n), decreasing_list (m + 1)
@@ -18,3 +21,30 @@ lemma decreasing_list_0_is_empty ( l : decreasing_list 0 ) : l = decreasing_list
 begin
   cases l, refl
 end
+
+structure Bijection ( U V : Type ) :=
+  ( morphism : U → V )
+  ( inverse  : V → U )
+  ( witness_1 : ∀ u : U, inverse (morphism u) = u )
+  ( witness_2 : ∀ v : V, morphism (inverse v) = v )
+
+class Finite ( α : Type ) :=
+  ( cardinality : nat )
+  ( bijection : Bijection α (fin cardinality) )
+
+
+
+def enumerate_decreasing_lists {n: ℕ} : Π (k : ℕ) (p : k < 2^n), decreasing_list n
+| k p := sorry
+
+definition decreasing_list_cardinality : Π {n : ℕ}, Finite (decreasing_list n)
+| 0       := {
+               cardinality := 1,
+               bijection   := {
+                morphism := λ l, 0,
+                inverse  := λ n, decreasing_list.nil,
+                witness_1 := begin intros, cases u, refl end,
+                witness_2 := begin intros, cases v, cases is_lt, refl, cases a, end
+               }
+             }
+| (n + 1) := sorry
