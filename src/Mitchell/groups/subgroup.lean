497599724fb64aef3f8f022fefc97d2e6f5c6619
Mitchell <u5796267@anu.edu.au>
2018-01-13 14:27:50 +1100

    Image of a subgroup is a subgroup

--- src/Mitchell/groups/subgroup.lean
+++ src/Mitchell/groups/subgroup.lean
@@ -1,10 +1,7 @@
+import data.set.basic
+
 universes u v w
 variables {α : Type u} {β : Type v} {γ : Type w}
-
-class subgroup {α : Type u} [group α] (S : set α) : Prop := 
-    (mul_closed : ∀ a b ∈ S, a * b ∈ S) (inv_closed : ∀ a ∈ S, a⁻¹ ∈ S) (one_closed : (1 : α) ∈ S)
-
--- theorem int_subgroups (S : set ℤ) [group ℤ] [subgroup S] : ∃ a ∈ S, ∀ b ∈ S, a ∣ b := sorry
 
 structure is_homomorphism [group α] [group β] (f : α → β) : Prop :=
     (hom_mul : ∀ a b, f (a * b) = (f a) * (f b))
@@ -12,8 +9,8 @@ structure is_homomorphism [group α] [group β] (f : α → β) : Prop :=
 attribute [simp] is_homomorphism.hom_mul
 
 namespace is_homomorphism
-variables [group α] [group β] [group γ]
-variables {f : α → β} {g : β → γ} {a : α}
+variables [group α] [group β]
+variables {f : α → β} {a : α}
 
 section
 variable (hf: is_homomorphism f)
@@ -35,3 +32,32 @@ calc
 end
 end is_homomorphism
 
+
+class subgroup [group α] (S : set α) : Prop := 
+    (one_closed : (1 : α) ∈ S)
+    (inv_closed : ∀ {a}, a ∈ S → a⁻¹ ∈ S) 
+    (mul_closed : ∀ {a b}, a ∈ S → b ∈ S → a * b ∈ S) 
+
+-- theorem int_subgroups (S : set ℤ) [group ℤ] [subgroup S] : ∃ a ∈ S, ∀ b ∈ S, a ∣ b := sorry
+
+namespace subgroup
+variables [group α] [group β]
+variables {S : set α} [subgroup S]
+
+section
+variables {x y : α}
+
+end
+
+
+instance image {f : α → β} (hf: is_homomorphism f) : subgroup (f '' S) :=
+    {
+        subgroup .
+        mul_closed := assume c₁ c₂ ⟨b₁, hb₁, eq₁⟩ ⟨b₂, hb₂, eq₂⟩,
+        ⟨b₁ * b₂, mul_closed hb₁ hb₂, by simp [eq₁, eq₂, hf.hom_mul]⟩,
+        one_closed := ⟨1, one_closed S, hf.one⟩,
+        inv_closed := assume a ⟨b, hb, eq⟩,
+        ⟨b⁻¹, inv_closed hb, by simp [eq, hf.inv]⟩ 
+    }
+
+end subgroup
