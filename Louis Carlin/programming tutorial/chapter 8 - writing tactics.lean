01534a6bfe6a31a9ccf892d025ae77e71cf9a831
L <ljc@cock.li>
2018-01-17 18:21:41 +1100

    8.2

--- Louis Carlin/programming tutorial/chapter 8 - writing tactics.lean
+++ Louis Carlin/programming tutorial/chapter 8 - writing tactics.lean
@@ -1,5 +1,7 @@
 /- 8.1 - First look -/
 
+namespace one
+
 open tactic
 
 variables a b : Prop
@@ -82,3 +84,100 @@ do
 
 example : a → b → a ∧ b :=
 by my_tactic
+
+end one
+
+/- 8.2 - Names and Expressions -/
+namespace two
+open tactic
+
+example (a b : Prop ) (h : a ∧ b) : b ∧ a :=
+begin
+   split,
+   exact and.right h,
+   exact and.left h 
+end
+
+example (a b : Prop ) (h : a ∧ b) : b ∧ a :=
+by do split,
+    to_expr ```(and.right h) >>= exact,
+    to_expr ```(and.left h) >>= exact
+
+/-
+name - hierarchical names
+expre - expressions
+pexpr - pre-expressions
+-/
+
+-- names are used as identifiers that reference defined constants in Lean (also local variables, atrributes, and other objects)
+namespace foo
+theorem bar : true := trivial
+
+meta def my_tac : tactic unit := mk_const `bar >>= exact
+
+example : true := by my_tac -- problem is propper name is `foo.bar rather than `bar
+-- we can also use double backtick to ask the parser to resolve the expression with the name of an object
+-- in the environment at parse time
+
+end foo
+
+-- necessary to generate a fresh name
+example (a : Prop) : a → a :=
+by do n ← mk_fresh_name,
+    intro n,
+    hyp ← get_local n, -- what is going on?
+    local_context >>= trace,
+    exact hyp
+
+
+-- the purpose of tactic mode is to construct expressions
+
+example (a b : Prop) (h : a ∧ b) : b ∧ a :=
+by do
+    split,
+    eh ← get_local `h,
+    mk_mapp ``and.right [none, none, some eh] >>= exact, -- mk_mapp retrieves the definition of and.right
+    mk_mapp ``and.left [none, none, some eh] >>= exact -- first arg is a name, second is a "list (option expr)"
+             -- none entries tell mk_mapp to treat that argument as implicit and infer it with type inference
+
+
+example (a b : Prop) (h : a ∧ b) : b ∧ a :=
+by do
+    split,
+    ea← get_local `a,
+    eb ← get_local `b,
+    eh ← get_local `h,
+    mk_app ``and.right [ea, eb, eh] >>= exact, -- can only include explicit arguments?
+    mk_app ``and.left [ea, eb, eh] >>= exact
+end two
+
+open tactic
+
+example (a b : Prop) (h : a ∧ b) : b ∧ a :=
+by do
+    split,
+    eh ← get_local `h,
+    mk_const ``and.right >>= apply,
+    exact eh,
+    -- target >>= trace, -- a
+    mk_const ``and.left >>= apply,
+    -- target >>= trace, -- a ∧ ?m_1
+    exact eh
+
+example (a b : Prop) (h : a ∧ b) : b ∧ a :=
+by do
+    split,
+    to_expr ```(and.right h) >>= exact, -- ```(...) constructs pexpr
+    to_expr ```(and.left h) >>= exact
+
+example (a b : Prop) (h : a ∧ b) : b ∧ a :=
+by do
+    split,
+    eh ← get_local `h,
+    to_expr ``(and.right %%eh) >>= exact, -- insert expression eh intro pre-expression with %%
+    to_expr ``(and.left%%eh) >>= exact
+
+    -- ``(...) -- resolves names when tactic is defined
+    -- ``(...) -- resolves names when tactic is executed
+
+/- 8.3 - Examples -/
