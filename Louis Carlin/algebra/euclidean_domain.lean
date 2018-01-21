7647393037af3a27c5c8b82172c75f7d2f2bbfc6
Louis Carlin <u6384109@anu.edu.au>
2018-01-21 22:13:22 +1100

    Started trying to show integers form a euclidean domain

--- Louis Carlin/algebra/euclidean_domain.lean
+++ Louis Carlin/algebra/euclidean_domain.lean
@@ -1,3 +1,4 @@
+import data.int.basic
 -- TODO
 -- examples
 -- polynomials with ED coefficients are a ED
@@ -22,13 +23,54 @@ class euclidean_domain (α : Type u) extends integral_domain α :=
 
 ( witness : ∀ a b, (quotient a b) * b + (remainder a b) = a )
 
-( valuation : ∃ f : α → ℕ, ∀ a b, f(remainder a b) < f b )
+( valuation : ∃ f : α → ℕ, ∀ a b, b = 0 ∨ f(remainder a b) < f b ) -- is using an or statement the nicest way to define this?
+
+set_option trace.class_instances true
 
 example : integral_domain ℤ := by apply_instance
 
+def a : integral_domain ℤ := by apply_instance
+#reduce a.one
+#check a.mul
+#reduce a.zero
+
+set_option trace.class_instances false
+
+open int
+open classical
+
+-- theorem mod_lt_of_pos (a : ℤ) {b : ℤ} (H : b > 0) : a % b < b
+
+#reduce (5:int)/(0:int)
+#reduce (5:int)%(0:int)
 
 instance int_euclidean_domain : euclidean_domain ℤ :=
 {
-    
+    a with 
+        quotient := λ x y, x / y,
+        remainder := λ x y, x % y,
+        witness := begin
+                    intros,
+                    rw [add_comm, mul_comm],
+                    exact mod_add_div a b,
+                   end,
+        valuation := begin
+                        existsi (λ x, nat_abs x),
+                        intros,
+                        simp,
+                        cases em (b=0), 
+                        {
+                            left,
+                            exact h
+                        },
+                        {
+                            right,
+                            admit
+                        -- mod_lt a h,
+                        
+                        }
+
+                     end
 }  
+
 
