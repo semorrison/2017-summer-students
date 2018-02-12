44402f22e6ca777bc83137cf0b1e864af511f18e
Louis Carlin <u6384109@anu.edu.au>
2018-02-12 14:50:02 +1100

    proved a bunch of lemmas

--- Louis Carlin/algebra/euclidean_domain_rewrite.lean
+++ Louis Carlin/algebra/euclidean_domain_rewrite.lean
@@ -8,6 +8,33 @@ import tactic.ring
 
 universes u v
 
+def lt_wf : (well_founded nat.lt) := -- will need to be replaced by more general well_founded
+    begin
+      split, intro a, induction a with b h,
+      {
+          split,
+          intro y,
+          intro h,
+          cases h,
+      },
+      {
+        split,
+        intro y,
+        intro h,
+        cases h,
+        {
+            assumption
+        },
+        {
+            have p : y < b, by sorry,
+            cases h,
+            exact h_h y p,
+        }
+      }
+    end
+
+
+
 definition euclidean_valuation {α} [has_zero α] (r : α → α → α) := { f : α → ℕ // ∀ a b, b = 0 ∨ has_well_founded.r (f(r a b)) (f b)}
 
 class euclidean_domain (α : Type) extends integral_domain α :=
@@ -16,14 +43,7 @@ class euclidean_domain (α : Type) extends integral_domain α :=
 ( witness : ∀ a b, (quotient a b) * b + (remainder a b) = a )
 ( valuation : euclidean_valuation remainder)
 
-/- 
-Wikipedia suggests defining a valuation with the property "For all nonzero a and b in α, f(a) ≤ f(ab)".
--/
-def valuation' {α : Type} [ed : euclidean_domain α] : euclidean_valuation (ed.remainder) :=
-{
-    val := λ a, {n : nat | ∃ x, n = ed.valuation.val (x*a)},
-    property := sorry,
-}
+
 
 def p : ℕ → Prop := λ n, n > 10
 
@@ -46,10 +66,6 @@ begin
     exact this a,
 end
 
-
-
-
-
 instance euclidean_domain_has_div {α : Type} [euclidean_domain α] : has_div α := {
     div := euclidean_domain.quotient
 }
@@ -57,6 +73,40 @@ instance euclidean_domain_has_div {α : Type} [euclidean_domain α] : has_div α
 instance euclidean_domain_has_mod {α : Type} [euclidean_domain α] : has_mod α := {
     mod := euclidean_domain.remainder
 }
+
+
+
+/- 
+Wikipedia suggests defining a valuation with the property "For all nonzero a and b in α, f(a) ≤ f(ab)".
+-/
+noncomputable def valuation' {α : Type} [ed : decidable_euclidean_domain α] : euclidean_valuation (ed.remainder) := 
+{ -- you could maybe get around this requiring decidable_euclidean_domain by using em since this is already non-computable
+    val := λ a, if a = 0 then 0 else well_founded.min lt_wf {n : nat | ∃ x, x ≠ 0 ∧ n = ed.valuation.val (x*a)} 
+    begin
+        have fin :ed.valuation.val (1*a) ∈ {n : nat | ∃ x, x ≠ 0 ∧ n = ed.valuation.val (x*a)},
+        simp,
+        existsi (1:α),
+        split,
+        exact one_ne_zero,
+        simp,
+        exact set.ne_empty_of_mem fin,
+    end,
+    property :=
+    begin
+        intros,
+        cases decidable.em (b=0),
+        left, exact h,
+        right,
+        simp [h],
+        cases decidable.em (euclidean_domain.remainder a b = 0),
+        simp [h_1],
+        sorry,
+        sorry,
+    end,
+}
+
+lemma valuation'_property_2 {α : Type} [ed : decidable_euclidean_domain α] : ∀ a b : α, a = 0 ∨ b = 0 ∨ has_well_founded.r (valuation'.val a) (valuation'.val (a*b)) :=
+sorry
 
 
 instance ed_has_sizeof {α : Type} [ed:decidable_euclidean_domain α] : has_sizeof α := {
@@ -76,13 +126,6 @@ instance ed_has_sizeof {α : Type} [ed:decidable_euclidean_domain α] : has_size
 
 
 /- lemmas -/
-@[simp] lemma mod_zero {α : Type} [ed : euclidean_domain α] (a : α)  : a % 0 = a :=
-begin
-    have := ed.witness,
-    have := this a 0,
-    simp at this,
-    exact this,
-end
 
 def gcd {α : Type} [ed : decidable_euclidean_domain α] : α → α → α
 | x y := if x_zero : x = 0 then y
@@ -117,6 +160,77 @@ begin
     rw gcd,
     simp [h],
 end
+
+@[simp] lemma mod_zero {α : Type} [ed : euclidean_domain α] (a : α)  : a % 0 = a :=
+begin
+    have := ed.witness,
+    have := this a 0,
+    simp at this,
+    exact this,
+end
+
+lemma valuation'_lt_one {α : Type} [ed : decidable_euclidean_domain α] (x : α) : 
+has_well_founded.r (valuation'.val x) (valuation'.val (1:α)) → x = 0 :=
+begin
+    intro,
+    have := valuation'_property_2 x 1,
+    cases this, exact this,
+    cases this, have := one_ne_zero, contradiction,
+    simp at this, sorry -- wf contradiction
+end
+
+@[simp] lemma mod_one {α : Type} [decidable_euclidean_domain α] (x : α) : x % 1 = 0 :=
+begin
+    have := valuation'.property x 1,
+    cases this, have := one_ne_zero, contradiction,
+    exact valuation'_lt_one (x % 1) this,
+end 
+
+@[simp] lemma zero_mod  {α : Type} [ed : decidable_euclidean_domain α] (b : α) : 0 % b = 0 :=
+begin
+    have := ed.witness,
+    have h1:= this 0 b,
+    have h2 : euclidean_domain.remainder 0 b = b * (-euclidean_domain.quotient 0 b ), from sorry,
+    have := valuation'_property_2 b (-euclidean_domain.quotient 0 b),
+    cases this,
+    {
+        rw this_1, exact mod_zero (0:α)
+    },
+    {
+        cases this_1, 
+        {
+            simp at this_1, rw this_1 at h1, simp at h1,
+            dsimp [(%)], 
+            exact h1
+        },
+        {
+            have := valuation'.property 0 b,
+            rw ←h2 at this_1,
+            cases this,
+            rw this_2, exact mod_zero (0:α),
+            sorry -- contradiction between this_1 and this_2
+        }
+    }
+end
+
+@[simp] lemma zero_div' {α : Type} [ed : decidable_euclidean_domain α] (b : α) : b = 0 ∨ 0 / b = 0 :=
+begin
+    have := ed.witness,
+    have := this 0 b,
+    have h1 := zero_mod b, dsimp [(%)] at h1,
+    rw h1 at this,
+    simp at this,
+    dsimp [(/)],
+    cases decidable.em (b=0),
+    left, exact h,
+    right,
+    have := eq_zero_or_eq_zero_of_mul_eq_zero this,
+    cases this,
+    exact this_1,
+    contradiction,
+end
+
+#check no_zero_divisors
 
 @[simp] theorem gcd_one_left {α : Type} [decidable_euclidean_domain α] (n : α) : gcd 1 n = 1 := 
 begin
