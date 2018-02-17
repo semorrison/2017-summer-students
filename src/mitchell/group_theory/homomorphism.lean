39b4aeb5295714b72e5abf8132903e1687551853
Mitchell <u5796267@anu.edu.au>
2018-02-18 10:29:00 +1100

    Minor syntax changes

--- src/mitchell/group_theory/homomorphism.lean
+++ src/mitchell/group_theory/homomorphism.lean
@@ -1,37 +1,33 @@
-/-
-    Many of these basic definitions/proofs are modelled on similar definitions/proofs
-    in mathlib's module.lean
--/
-
 import data.set.basic init.function algebra.group
 
 universes u v w
-variables {α : Type u} {β : Type v} {γ : Type w}
+variables {G : Type u} {H : Type v} {K : Type w}
 
-class is_hom [group α] [group β] (f : α → β) : Prop :=
+class group_hom [group G] [group H] (f : G → H) : Prop :=
     (hom_mul : ∀ a b, f (a * b) = f a * f b)
 
-attribute [simp] is_hom.hom_mul
+attribute [simp] group_hom.hom_mul
 
-namespace is_hom
-variables [group α] [group β] [group γ]
+namespace group_hom
+variables [group G] [group H] [group K]
 
 section
+variables (f : G → H) [group_hom f]
 
 @[simp]
-lemma one {f : α → β} (hf : is_hom f) : f 1 = 1 := 
-mul_self_iff_eq_one.1 $ by rw ← hom_mul f (1 : α) (1 : α); simp
+lemma one : f 1 = 1 := 
+mul_self_iff_eq_one.1 $ by rw ← hom_mul f (1 : G) (1 : G); simp
 
 @[simp]
-lemma inv {f : α → β} (hf : is_hom f) (a : α)  : f a⁻¹ = (f a)⁻¹ :=
-eq.symm $ inv_eq_of_mul_eq_one $ by rw ← hom_mul f a a⁻¹; simp [hf.one]
+lemma inv (a : G)  : f a⁻¹ = (f a)⁻¹ :=
+eq.symm $ inv_eq_of_mul_eq_one $ by rw ← hom_mul f a a⁻¹; simp [one f]
 
 end
 
-lemma comp {g : β → γ} {f : α → β} (hg : is_hom g) (hf : is_hom f) : is_hom (g ∘ f) :=
+lemma comp (g : H → K) (f : G → H) [group_hom g] [group_hom f] : group_hom (g ∘ f) :=
 {   hom_mul := λ x y, calc
-    g (f (x * y)) = g (f x * f y)       : by simp [hf.hom_mul]
-    ...           = g (f x) * g (f y)   : by simp [hg.hom_mul]
+    g (f (x * y)) = g (f x * f y)       : by simp [group_hom.hom_mul f]
+    ...           = g (f x) * g (f y)   : by simp [group_hom.hom_mul g]
 }
 
-end is_hom
+end group_hom
